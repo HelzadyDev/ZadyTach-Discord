f6466c954b4fd474e7b450c92d72e261f877a7fe
HelzadyDev <natanael.santos.play@gmail.com>
2026-05-16 21:38:07 -0300

    feat(functions): exportar novos utilitários no index principal
    Atualiza src/functions/index.ts adicionando exportações
    dos novos módulos utilitários do sistema.
    
    Módulos exportados:
    - channel
    - commands
    - emojis
    - members
    - message
    
    Detalhes técnicos:
    - Centraliza os exports dos utilitários em um único ponto
    - Facilita imports externos usando barrel pattern
    - Mantém padronização de imports ESM com extensão .js
    - Melhora organização e escalabilidade da estrutura functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,7 @@
 export * from "./components/separator.js"
+
+export * from "./channel.js"
+export * from "./commands.js"
+export * from "./emojis.js"
+export * from "./members.js"
+export * from "./message.js"
